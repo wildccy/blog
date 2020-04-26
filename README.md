a96db8f0d30f1ef6aca96cc100b29a10674820c4
wildccy <chengye_a@163.com>
2020-04-26 19:45:51 +0800

    feat(hola):  update md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cheng
+
+![]()
+
+![]()
+
+Design & development records of personal site
+
+## Dev
+
+先列举下这次个站开发使用到的一些主要的包技术栈：
+
+**前端**
+
+- `React` + `Redux` ( `dva` )
+- `Webpack` ( `Roadhog` )
+- `Styled Components`
+
+**后端**
+
+- `NodeJs` + `Koa` + `MySQL`
+
+## Links
+
+- 🍃 <https://canisminor.cc>
--- src/utils/setTitle.js
+++ src/utils/setTitle.js
@@ -1,10 +1,10 @@
 export default (title, desc) => {
   const meta = document.getElementsByTagName('meta');
   if (title) {
-    title = `${title} - WildCY`;
+    title = `${title} - Cheng`;
     document.title = title;
   } else {
-    title = 'WildCY';
+    title = 'Cheng';
     document.title = title;
   }
 };
